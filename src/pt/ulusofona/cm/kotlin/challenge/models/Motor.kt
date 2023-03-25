ef8a8d1bb6c41523630e6ab4f3c33a8616479741
josesobral22005813 <a22005813@alunos.ulht.pt>
2023-03-25 00:43:54 +0000

    Programming at 1 am is not a good idea

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -5,7 +5,7 @@ import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 
 class Motor(var cavalos: Int, var cilindrada: Int) : Ligavel {
-    private var ligado = true
+    private var ligado = false
 
     override fun ligar() {
         if(ligado){
